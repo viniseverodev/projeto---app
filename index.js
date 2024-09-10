2a0c90ccd10364e9abbc5bd1db9fa674412932a6
Vinicius Severo <yvinisevero@gmail.com>
2024-09-10 16:15:46 -0300

    starting the application menu

---
+++ index.js
@@ -0,0 +1,40 @@
+const { select } = require ('@inquirer/prompts')
+
+const start = async () => {
+
+  while(true){
+
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "Cadastrar"
+        },
+        {
+          name: "Listar metas",
+          value: "Listar"
+        },
+        {
+          name: "Sair",
+          value: "Sair"
+        }
+      ]
+    })
+
+    switch(opcao) {
+      case "Cadastrar":
+        console.log("Vamos cadastrar")
+        break
+      case "Listar":
+        console.log("Vamos listar")
+        break
+      case "Sair":
+        console.log("Até a próxima!")
+        return
+    }
+
+  }
+}
+
+start()
